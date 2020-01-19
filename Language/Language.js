9623802404f7f7d9de627e0b528298d12473e971
danillog <danillog821@gmail.com>
2020-01-19 14:15:13 -0400

    3 files modified

--- Language/Language.js
+++ Language/Language.js
@@ -5,16 +5,16 @@ class Language extends Component {
   render() {
     return (
       <div>
-        <div class="row align-items-center">
+        <div class="linha row align-items-center">
           <div class="box col mx-auto text-center">
             <img
               class="logo"
               src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/91436790925937.5e24891fa5818.png"
             />
-            <h1> Enter in your language </ h1>
+            <h1> Enter your language </ h1>
           </div>
         </div>
-        <div class="row align-items-center">
+        <div class="row space align-items-center">
           <div class="box col mx-auto text-center">
             <img class="country" src="https://www.countryflags.io/us/shiny/64.png"/>
             <h2>English</h2>
