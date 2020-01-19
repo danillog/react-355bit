213ad340b478d74b66b6403c23a78c4060174692
danillog <danillog821@gmail.com>
2020-01-19 13:39:51 -0400

    2 files modified

--- Language/Language.js
+++ Language/Language.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
-import './Language.css';
+import React, { Component } from "react";
+import "./Language.css";
 
 class Language extends Component {
   render() {
     return (
-    <div class="frame">
-      <div class="box">
+      <div>
+        <div class="row align-items-center">
+          <div class="box col mx-auto text-center">
+            <img
+              class="img-responsive"
+              src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/91436790925937.5e24891fa5818.png"
+            />
+          </div>
+        </div>
+        <div class="row align-items-center">
+          <div class="col mx-auto text-center"></div>
+          <div class="col mx-auto text-center"></div>
+          <div class="col mx-auto text-center"></div>
+        </div>
       </div>
-    </div>
-      
     );
   }
 }
---
+++ Logo/Logo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Logo.css';
+
+class Logo extends Component {
+  render() {
+    return (
+    <div class="box">
+      <img class="img-responsive" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/91436790925937.5e24891fa5818.png"/>
+    </div>
+      
+    );
+  }
+}
+
+export default Logo;
