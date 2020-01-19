ca0a3ca15cddfb1fabd7b42143a3a8b9c733823a
danillog <danillog821@gmail.com>
2020-01-19 13:57:59 -0400

    3 files modified

--- Language/Language.js
+++ Language/Language.js
@@ -8,15 +8,25 @@ class Language extends Component {
         <div class="row align-items-center">
           <div class="box col mx-auto text-center">
             <img
-              class="img-responsive"
+              class="logo"
               src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/91436790925937.5e24891fa5818.png"
             />
+            <h1> Enter in your language </ h1>
           </div>
         </div>
         <div class="row align-items-center">
-          <div class="col mx-auto text-center"></div>
-          <div class="col mx-auto text-center"></div>
-          <div class="col mx-auto text-center"></div>
+          <div class="box col mx-auto text-center">
+            <img class="country" src="https://www.countryflags.io/us/shiny/64.png"/>
+            <h2>English</h2>
+          </div>
+          <div class="box col mx-auto text-center">
+            <img class="country" src="https://www.countryflags.io/es/shiny/64.png"/>
+            <h2>Español</h2>
+          </div>
+          <div class="box col mx-auto text-center">
+          <img class="country" src="https://www.countryflags.io/br/shiny/64.png"/>
+          <h2>Português</h2>
+          </div>
         </div>
       </div>
     );
