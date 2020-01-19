9a4333b7371c04826c74ddbc95b96c2cdbc8b472
danillog <danillog821@gmail.com>
2020-01-19 13:28:03 -0400

    1 file added

---
+++ Language/Language.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Language.css';
+
+class Language extends Component {
+  render() {
+    return (
+    <div class="frame">
+      <div class="box">
+      </div>
+    </div>
+      
+    );
+  }
+}
+
+export default Language;
