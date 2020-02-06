a1a69caa3d8bb5bfcf7aeea636a9be421b8a72cb
danillog <danillog821@gmail.com>
2020-02-06 07:39:48 -0400

    Mudança no css de Language

--- src/Language/Language.js
+++ src/Language/Language.js
@@ -5,23 +5,22 @@ import "./Language.css";
 class Language extends Component {
   render() {
     return (
-      <div>
+      <div class="box-main container">
         <div class="linha row align-items-center">
-          <div class="box col mx-auto text-center">
+          <div class="box-logo col mx-auto text-center">
             <img class="logo" src={Logo}/>
-            <h1> Enter in your language </ h1>
           </div>
         </div>
         <div class="row space align-items-center">
-          <div class="box col mx-auto text-center">
+          <div class="box-language col mx-auto text-center">
             <img class="country" src="https://www.countryflags.io/us/shiny/64.png"/>
             <h2>English</h2>
           </div>
-          <div class="box col mx-auto text-center">
+          <div class="box-language col mx-auto text-center">
             <img class="country" src="https://www.countryflags.io/es/shiny/64.png"/>
             <h2>Español</h2>
           </div>
-          <div class="box col mx-auto text-center">
+          <div class="box-language col mx-auto text-center">
           <img class="country" src="https://www.countryflags.io/br/shiny/64.png"/>
           <h2>Português</h2>
           </div>
